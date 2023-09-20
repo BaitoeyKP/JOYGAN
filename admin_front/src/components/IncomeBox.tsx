e94761202042c23d42880685e659bae3bcbde005
BaitoeyKP <kanyarat_toey@hotmail.com>
2023-09-20 12:36:12 +0700

    incomeBox components

--- admin_front/src/components/IncomeBox.tsx
+++ admin_front/src/components/IncomeBox.tsx
@@ -1,15 +1,23 @@
 import React from "react"
 
 interface type {
-    date: string,
-    income: number
+    data: {
+        date: string;
+        income: number;
+    }[];
 }
 
-function IncomeBox({ date, income }: type) {
+function IncomeBox({ data }: type) {
     return (
-        <div className="w-4/5 bg-white drop-shadow-md rounded-xl flex justify-between items-center py-6 text-xl px-10">
-            <p><span>วันที่ </span><span>{date}</span></p>
-            <p><span>{income}</span><span> บาท</span></p>
+        <div className="w-4/5  grid gap-y-6">
+            {data.map((info, index) => {
+                return (
+                    <div key={index} className="bg-white drop-shadow-md rounded-xl flex justify-between items-center py-8 text-xl px-10">
+                        <p><span>วันที่ </span><span>{info.date}</span></p>
+                        <p><span>{info.income}</span><span> บาท</span></p>
+                    </div>
+                );
+            })}
         </div>
     )
 }
--- admin_front/src/pages/Income.tsx
+++ admin_front/src/pages/Income.tsx
@@ -1,9 +1,16 @@
 import IncomeBox from "../components/IncomeBox"
 
+const data = [
+    {date:"16 กรกฎาคม 2562", income:9990},
+    {date:"16 กรกฎาคม 2562", income:9990},
+    {date:"16 กรกฎาคม 2562", income:9990},
+    {date:"16 กรกฎาคม 2562", income:9990},
+]
+
 function Income() {
     return (
         <div className="bg-cream-bg h-screen w-screen">
-            <div className="flex justify-center"><IncomeBox date={"16 กรกฎาคม 2562"} income={9990}></IncomeBox></div>
+            <div className="flex justify-center"><IncomeBox data={data}></IncomeBox></div>
         </div>
     )
 }
