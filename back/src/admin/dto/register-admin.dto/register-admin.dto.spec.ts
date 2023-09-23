3401e56d2f3077cfd4e93eb8ee1867a4ff7a50b0
Kampanatkc <88760558+kampanatchinnarach@users.noreply.github.com>
2023-09-23 23:12:33 +0700

    reset

--- back/src/admin/dto/register-admin.dto/register-admin.dto.spec.ts
+++
@@ -1,7 +0,0 @@
-import { RegisterAdminDto } from './register-admin.dto';
-
-describe('RegisterAdminDto', () => {
-  it('should be defined', () => {
-    expect(new RegisterAdminDto()).toBeDefined();
-  });
-});
