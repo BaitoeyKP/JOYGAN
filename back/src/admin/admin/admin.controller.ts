dffd9d8430fe0d904c30b7502f7f7cb451472369
Kampanatkc <88760558+kampanatchinnarach@users.noreply.github.com>
2023-09-26 21:45:10 +0700

    test

--- back/src/admin/admin/admin.controller.ts
+++ back/src/admin/admin/admin.controller.ts
@@ -1,11 +1,22 @@
 import { Body, Controller, Get, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { Admin } from 'src/typeorm/admin.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateAdminDto } from '../dto/CreateAdmin.dto';
+
 
 
 @Controller('admin/user')
 export class AdminController {
- 
+    constructor(
+        private readonly adminService : AdminService
+        ){}
+         @UsePipes(ValidationPipe)
+        @Post("register")
+        createNewAdmin(@Body() createAdminDto : CreateAdminDto){
+            return this.adminService.createAdmin(createAdminDto)
+
+        }
 }
 
 
--- back/src/admin/admin/admin.service.ts
+++ back/src/admin/admin/admin.service.ts
@@ -1,10 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { forwardRef,Injectable, Post } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Admin } from 'src/typeorm';
 import { Repository } from 'typeorm';
+import { CreateAdminDto } from '../dto/CreateAdmin.dto';
+
 
 
 @Injectable()
 export class AdminService {
  
+  constructor(
+    @InjectRepository(Admin) private readonly adminRepository: Repository<Admin>,   
+  ){}
+
+  
+  
+  createAdmin(createAdminDto:CreateAdminDto){
+    const newAdmin = this.adminRepository.create(createAdminDto)
+    return this.adminRepository.save(newAdmin)
+  }
+
 }
