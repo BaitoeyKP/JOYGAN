1c1e13c8f1f269f228d9f17d36f7428c6fcd3d98
Kampanatkc <88760558+kampanatchinnarach@users.noreply.github.com>
2023-09-26 19:47:14 +0700

    reset

--- back/src/admin/admin/admin.controller.ts
+++ back/src/admin/admin/admin.controller.ts
@@ -5,21 +5,7 @@ import { Admin } from 'src/typeorm/admin.entity';
 
 @Controller('admin/user')
 export class AdminController {
-    constructor(private adminService: AdminService) {}
-
-    @Post('register')
-    async register(@Body() adminData: Partial<Admin>) {
-        // ตรวจสอบว่ามีผู้ดูแลระบบที่มีชื่อผู้ใช้เหมือนกันหรือไม่
-        const existingAdmin = await this.adminService.findOne(adminData.admin_username);
-        if (existingAdmin) {
-            return { message: 'ชื่อผู้ใช้นี้ถูกใช้แล้ว' };
-        }
-
-        // สร้างผู้ดูแลระบบใหม่และบันทึกลงในฐานข้อมูล
-        const admin = await this.adminService.create(adminData);
-        return { message: 'ลงทะเบียนสำเร็จ', admin };
-    }
-
+ 
 }
 
 
--- back/src/admin/admin/admin.service.ts
+++ back/src/admin/admin/admin.service.ts
@@ -2,23 +2,9 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Admin } from 'src/typeorm';
 import { Repository } from 'typeorm';
-import * as bcrypt from 'bcrypt';
+
 
 @Injectable()
 export class AdminService {
-    constructor(
-        @InjectRepository(Admin)
-        private adminRepository: Repository<Admin>,
-      ) {}
-    
-      
-    async create(adminData: Partial<Admin>): Promise<Admin> {
-        const admin = this.adminRepository.create(adminData);
-        return this.adminRepository.save(admin);
-    }
-
-    async findOne(username: string): Promise<Admin | undefined> {
-        return this.adminRepository.findOne({ where: { admin_username: username } });
-    }
-    
+ 
 }
