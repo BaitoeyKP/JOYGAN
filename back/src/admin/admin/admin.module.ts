0f5048b9c9dcbc29b715fac23126bcaa83b1cf2d
oatpt <oatza142536@gmail.com>
2023-09-26 22:56:06 +0700

    admin

--- back/src/admin/admin/admin.module.ts
+++ back/src/admin/admin/admin.module.ts
@@ -1,8 +1,7 @@
 import {  Module ,forwardRef  } from '@nestjs/common';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Admin } from 'src/typeorm';
+
 
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Admin } from 'src/typeorm';
