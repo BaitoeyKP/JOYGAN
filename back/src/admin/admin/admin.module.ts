46d78762cc3fdf13ca515d9bb8b984e06ed127e0
palmzaawow <palmrock4@gmail.com>
2023-09-26 23:55:26 +0700

    ver6

--- back/src/admin/admin/admin.module.ts
+++ back/src/admin/admin/admin.module.ts
@@ -2,7 +2,7 @@ import {  Module } from '@nestjs/common';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Admin } from 'src/typeorm';
+import { Admin } from 'src/typeorm/admin.entity';
 
 
 @Module({
