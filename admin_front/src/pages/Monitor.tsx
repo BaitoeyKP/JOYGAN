f57201a75a423734bbac5708ccccffb39e906c97
BaitoeyKP <kanyarat_toey@hotmail.com>
2023-09-27 10:53:55 +0700

    import pic

--- admin_front/src/pages/Monitor.tsx
+++ admin_front/src/pages/Monitor.tsx
@@ -1,8 +1,20 @@
+// import qrCode from "../assets/qrCode.png"
+
 function Monitor() {
     return (
-        <div>
-            <div></div>
-            <div></div>
+        <div className="flex bg-cream-bg min-h-screen h-full w-screen">
+            <div className="flex basis-1/2 justify-center items-center">
+
+            </div>
+            <div className="flex basis-1/2 justify-center items-center flex-col">
+                <div className="flex basis-1/2 justify-center items-center">
+                    <h1 className="text-5xl font-bold font-black-text">ยอดสนับสนุนสูงสุด</h1>
+                </div>
+                <div className="flex basis-1/2 justify-center items-center">
+                    <img src={qrCode} alt="" />
+                    <p className="text-5xl font-bold font-black-text">ดาวน์โหลดแอปพลิเคชั่น</p>
+                </div>
+            </div>
         </div>
     )
 }
