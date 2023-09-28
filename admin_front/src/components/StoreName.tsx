4a6aeb2443e8239fdbc8a87b0d985932a67b07a1
Sawat Lapprasertlum <sawat.kia2000@gmail.com>
2023-09-28 15:48:08 +0700

    add edit icon

--- admin_front/src/components/StoreName.tsx
+++ admin_front/src/components/StoreName.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 interface Store {
   name: string;
@@ -11,10 +11,29 @@ interface StoreNameProps {
 
 const StoreName: React.FC<StoreNameProps> = ({ store }) => {
   return (
-    <h1 className="text-4xl">
+    <h1 className="text-4xl flex flex-row items-end">
       ร้าน
       <span className="text-5xl text-dark-purple-highlight">{store.name}</span>
       <span>({store.code})</span>
+      <a href="#">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+      >
+        <g
+          fill="none"
+          stroke="#a3a0a0"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+        >
+          <path d="M7 7H6a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2-2v-1" />
+          <path d="M20.385 6.585a2.1 2.1 0 0 0-2.97-2.97L9 12v3h3l8.385-8.415zM16 5l3 3" />
+        </g>
+      </svg>
+      </a>
     </h1>
   );
 };
